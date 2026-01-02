975c0c4c3e9404d9e9a5352ea5fb39d6b64ed433
aerialdreamxl <yxdowncomputer@outlook.com>
2026-01-02 12:59:31 +0800

    fix build-all.sh

--- docker-quickenv/build-all.sh
+++ docker-quickenv/build-all.sh
@@ -1,2 +1,2 @@
-podman build -t aerialdreamxl/docker-quickenv:latest-base -f ./base.Dockerfile .
+docker build -t aerialdreamxl/docker-quickenv:latest-base -f ./base.Dockerfile .
 docker build -t aerialdreamxl/docker-quickenv:latest -f ./latest.Dockerfile .
