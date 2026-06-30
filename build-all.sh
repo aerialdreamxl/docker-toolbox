b97af4a9cd12f5dc05cd0f6e1077a473e7dcba1c
Aerodream Granite <yxdowncomputer@outlook.com>
2026-06-30 21:18:02 +0800

    make everything useful (again)

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,14 @@
 cd supervisor
 docker build -t aerialdreamxl/supervisord-toolbox:ubuntu -f Dockerfile.ubuntu .
 docker build -t aerialdreamxl/supervisord-toolbox:debian -f Dockerfile.debian .
+cd ..
+cd supervisor-cn
+docker build -t aerialdreamxl/supervisord-toolbox:ubuntu-cn -f Dockerfile.ubuntu .
+docker build -t aerialdreamxl/supervisord-toolbox:debian-cn -f Dockerfile.debian .
+cd ..
+cd hermes
+docker build -t aerialdreamxl/hermes-toolbox .
+cd ..
+cd devcontainer
+docker build -t aerialdreamxl/dev-toolbox .
+cd ..
