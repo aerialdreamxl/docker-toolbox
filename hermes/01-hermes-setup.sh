4edfc88a2d8dfc9128f26335f66cfa22fd9fe3da
Aerodream Granite <yxdowncomputer@outlook.com>
2026-08-18 10:05:43 +0800

    hermes: can choose publish or not

--- hermes/01-hermes-setup.sh
+++ hermes/01-hermes-setup.sh
@@ -1,3 +1,9 @@
 if [[ ! -f "${HERMES_HOME}"/config.yaml ]]; then
     gosu ubuntu:ubuntu /bin/bash -c "hermes setup"
 fi
+
+if [[ $TOOLBOX_HERMES_PUBLISH == 1 ]]; then
+    rm /etc/supervisor/conf.d/hermes-dashboard.conf /etc/supervisor/conf.d/hermes-dashboard-proxy.conf
+    mv /etc/supervisor/conf.d/hermes-dashboard-publish.conf.disabled /etc/supervisor/conf.d/hermes-dashboard.conf
+    echo "WARN: Published Hermes Agent Dashboard"
+fi
--- hermes/hermes-installer.sh
+++ hermes/hermes-installer.sh
@@ -44,7 +44,7 @@ BOLD='\033[1m'
 
 # Configuration
 REPO_URL_SSH="git@https://ghfast.top/github.com:NousResearch/hermes-agent.git"
-REPO_URL_HTTPS="https://ghfast.top/https://github.com/NousResearch/hermes-agent.git"
+REPO_URL_HTTPS="https://gh-proxy.com/https://github.com/NousResearch/hermes-agent.git"
 HERMES_HOME="${HERMES_HOME:-$HOME/.hermes}"
 # INSTALL_DIR is resolved AFTER arg parsing and OS detection so we can pick an
 # FHS-style layout for root installs.  Track whether the user gave us an
