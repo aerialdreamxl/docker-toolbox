1cf378ed1ce109b4e0ad02270dd0f723ca7f3200
Aerodream Granite <yxdowncomputer@outlook.com>
2026-08-19 15:20:46 +0800

    hermes: support hermes multiprofile

--- hermes/01-hermes-setup.sh
+++ hermes/01-hermes-setup.sh
@@ -7,3 +7,21 @@ if [[ $TOOLBOX_HERMES_PUBLISH == 1 ]]; then
     mv /etc/supervisor/conf.d/hermes-dashboard-publish.conf.disabled /etc/supervisor/conf.d/hermes-dashboard.conf
     echo "WARN: Published Hermes Agent Dashboard"
 fi
+
+if [[ -n $(ls -A "${HERMES_HOME}/profiles" 2>/dev/null) ]]; then
+    echo "INFO: Multi-profile setup detected, adding configuration..."
+    for profile in ${HERMES_HOME}/profiles/*/; do
+        profilename=$(basename $profile)
+        echo Added Profile: $profilename
+        cat <<EOF > /etc/supervisor/conf.d/hermes-gateway-$profilename.conf
+[program:hermes-gateway-$profilename]
+command=gosu ubuntu:ubuntu /bin/bash -c "hermes -p $profilename gateway run"
+directory=/home/ubuntu
+user=root
+autostart=true
+autorestart=true
+stdout_logfile=/var/log/supervisor/hermes-gateway-$profilename.log
+stderr_logfile=/var/log/supervisor/hermes-gateway-$profilename.log
+EOF
+    done
+fi
